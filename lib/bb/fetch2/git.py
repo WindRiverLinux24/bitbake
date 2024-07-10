99ea5c94103bdc378f64360d82895482d63fe17a
Robert Yang <liezhi.yang@windriver.com>
2024-07-10 00:12:07 -0700

    Revert "fetch2/git: Fix File name too long error"
    Issue: LINUXEXEC-32732
    
    The fix is incorrect which cuases bitbake-selftest failed, will use a new patch
    to fix it.
    
    This reverts commit a4b472d848cb420f280fad3dcd6412c379180080.
    
    (LOCAL REV: NOT UPSTREAM) -- Revert a local commit.
    
    Signed-off-by: Robert Yang <liezhi.yang@windriver.com>

--- lib/bb/fetch2/git.py
+++ lib/bb/fetch2/git.py
@@ -83,7 +83,6 @@ import subprocess
 import tempfile
 import bb
 import bb.progress
-import hashlib
 from contextlib import contextmanager
 from   bb.fetch2 import FetchMethod
 from   bb.fetch2 import runfetchcmd
@@ -278,10 +277,7 @@ class Git(FetchMethod):
                     ud.unresolvedrev[name] = ud.revisions[name]
                 ud.revisions[name] = self.latest_revision(ud, d, name)
 
-        if ud.proto == "file":
-            gitsrcname = '%s.%s' % (hashlib.md5(ud.path.encode('utf-8')).hexdigest(), os.path.basename(ud.path))
-        else:
-            gitsrcname = '%s%s' % (ud.host.replace(':', '.'), ud.path.replace('/', '.').replace('*', '.').replace(' ','_').replace('(', '_').replace(')', '_'))
+        gitsrcname = '%s%s' % (ud.host.replace(':', '.'), ud.path.replace('/', '.').replace('*', '.').replace(' ','_').replace('(', '_').replace(')', '_'))
         if gitsrcname.startswith('.'):
             gitsrcname = gitsrcname[1:]
 
